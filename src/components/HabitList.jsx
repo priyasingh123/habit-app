84b7763c2a506a7ab45fc3768a542c8c36b899a9
priya <priyaasinghh345@gmail.com>
2026-03-06 15:56:47 +0530

    add check box

--- src/components/HabitList.jsx
+++ src/components/HabitList.jsx
@@ -1,18 +1,48 @@
 import { Icon } from "@chakra-ui/react";
 import { fetchHabits } from "../mockData/habits";
-import { useEffect, useState } from "react";
-import { Check, CheckSquare, Square } from "lucide-react";
+import { useEffect, useState, useReducer } from "react";
+import { CheckSquare, Square } from "lucide-react";
 
 const HabitList = () => {
   const [habitList, setHabitList] = useState([]);
+  const initialTasks = {
+    habits: Array(habitList.length).fill(false),
+  };
+  const [tasks, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        return {
+          ...state,
+          habits: [...state.habits, false],
+        };
+      case "initialize": {
+        console.log("initialize", action.payload);
+        return {
+          ...state,
+          habits: Array(action.payload).fill(false),
+        };
+      }
+      case "toggle": {
+        const habitNumber = action.payload;
+        const arr = [...state.habits];
+        arr[habitNumber] = !arr[habitNumber];
+        return {
+          ...state,
+          habits: arr,
+        };
+      }
+    }
+  }, initialTasks);
+
   useEffect(() => {
-    const fetchHabitList = () => {
+    const fetchDetails = () => {
       const habits = fetchHabits();
-      console.log("habits", habits);
       setHabitList(habits);
+      dispatch({ type: "initialize", payload: habits.length });
     };
-    fetchHabitList();
+    fetchDetails();
   }, []);
+
   return (
     <div className="habit_list_container">
       {habitList.map((habit, index) => {
@@ -20,7 +50,10 @@ const HabitList = () => {
           <div className="habit" key={`${index}`}>
             <p className="habit_text">{habit}</p>
             <div className="check_icon">
-              <Icon as={Square} />
+              <Icon
+                as={tasks.habits[index] === false ? Square : CheckSquare}
+                onClick={() => dispatch({ type: "toggle", payload: index })}
+              />
             </div>
           </div>
         );
