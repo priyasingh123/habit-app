989211e808cedf99ec9b043941c1dbffddb62246
priya <priyaasinghh345@gmail.com>
2026-03-18 15:14:23 +0530

    close popup when clicked outside

--- src/components/ColorPopUp.jsx
+++ src/components/ColorPopUp.jsx
@@ -1,19 +1,37 @@
+import { useEffect, useRef } from "react";
 import { useColorStore } from "../store/useColorStore";
 
 const ColorPopUp = ({ setShowPalette }) => {
   const setColorTheme = useColorStore((state) => state.setColorTheme);
+  const popupRef = useRef(null);
+
   const colorsOptions = [
     { name: "red", value: "rgb(134, 21, 96)" },
     { name: "blue", value: "rgb(21, 85, 170)" },
     { name: "green", value: "rgb(21, 134, 96)" },
     { name: "yellow", value: "rgb(255, 255, 0)" },
   ];
+
   const handleClick = (color) => {
     setColorTheme(color);
     setShowPalette(false);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        setShowPalette(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [setShowPalette]);
+
   return (
-    <div className="color_container">
+    <div ref={popupRef} className="color_container">
       {colorsOptions.map((color) => (
         <div
           key={color.value}
