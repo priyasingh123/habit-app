ee4abc01c0ae4182ea2e2eb81f58665bec8d9f9c
priya <priyaasinghh345@gmail.com>
2026-04-15 10:58:51 +0530

    add missing code from Dayheader

--- src/components/DayHeader.jsx
+++ src/components/DayHeader.jsx
@@ -1,38 +1,37 @@
 import { colorTheme as theme } from "../utils/colorTheme";
 import { useColorStore } from "../store/useColorStore";
+import { forwardRef } from "react";
 
-export const DayHeader = ({
-  date,
-  setOpenDrawer,
-  setDrawerBody,
-  setMonthYear,
-}) => {
-  const colorTheme = useColorStore((state) => state.colorTheme);
-  const weekdays = ["S", "M", "T", "W", "T", "F", "S"];
-  const handleMonthClick = async (year, month) => {
-    // ref.current = performance.now();
-    setDrawerBody("monthlyStats");
-    setOpenDrawer(true);
-    setMonthYear({ month, year });
-  };
-  const { dayHeader } = theme[colorTheme];
-  return (
-    <>
-      <div>
-        <div
-          className="monthHeader"
-          onClick={() =>
-            handleMonthClick(date.getFullYear(), date.getMonth() + 1)
-          }
-        >
-          {`${date.toLocaleString("default", { month: "short" })} ${date.getFullYear()}`}
+export const DayHeader = forwardRef(
+  ({ date, setOpenDrawer, setDrawerBody, setMonthYear }, ref) => {
+    const colorTheme = useColorStore((state) => state.colorTheme);
+    const weekdays = ["S", "M", "T", "W", "T", "F", "S"];
+    const handleMonthClick = async (year, month) => {
+      ref.current = performance.now();
+      setDrawerBody("monthlyStats");
+      setOpenDrawer(true);
+      setMonthYear({ month, year });
+    };
+    const { dayHeader } = theme[colorTheme];
+    return (
+      <>
+        <div>
+          <div
+            className="monthHeader"
+            ref={ref}
+            onClick={() =>
+              handleMonthClick(date.getFullYear(), date.getMonth() + 1)
+            }
+          >
+            {`${date.toLocaleString("default", { month: "short" })} ${date.getFullYear()}`}
+          </div>
         </div>
-      </div>
-      <div className="day-header" style={{ "--dayHeader": dayHeader }}>
-        {weekdays.map((day, index) => (
-          <span key={`${day}-${index}`}>{day}</span>
-        ))}
-      </div>
-    </>
-  );
-};
+        <div className="day-header" style={{ "--dayHeader": dayHeader }}>
+          {weekdays.map((day, index) => (
+            <span key={`${day}-${index}`}>{day}</span>
+          ))}
+        </div>
+      </>
+    );
+  },
+);
