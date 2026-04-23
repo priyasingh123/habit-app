44a814e5d677b7a6add27c906e344ff0ff8b9bc0
priya <priyaasinghh345@gmail.com>
2026-04-23 11:47:29 +0530

    test for habit drawer

--- tests/e2e/habitDrawer.spec.ts
+++ tests/e2e/habitDrawer.spec.ts
@@ -1,10 +1,50 @@
 import { test, expect } from "@playwright/test";
 
 test.describe(() => {
-  test("should open habit drawer when date is clicked", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("/habit-app/");
-    await page.getByTestId("day").first().click();
+  });
+
+  test("should open habit drawer when date is clicked", async ({ page }) => {
+    await page
+      .getByTestId("day")
+      .filter({ hasText: /^[1-9]|[12][0-9]|3[01]$/ })
+      .first()
+      .click();
     const drawer = page.getByTestId("habit-drawer");
     await expect(drawer).toBeVisible({ timeout: 5000 });
+  });
+
+  test("should close habit drawer when clicked anywhere outside the drawer", async ({
+    page,
+  }) => {
+    await page
+      .getByTestId("day")
+      .filter({ hasText: /^[1-9]|[12][0-9]|3[01]$/ })
+      .first()
+      .click();
+    const drawer = page.getByTestId("habit-drawer");
+    await expect(drawer).toBeVisible({ timeout: 5000 });
+    await page.mouse.click(0, 0);
+    await expect(drawer).not.toBeVisible({ timeout: 5000 });
+  });
+
+  test("should show same date on drawer which date is clicked", async ({
+    page,
+  }) => {
+    await page.getByTestId("day").filter({ hasText: /22/ }).first().click();
+    const previousMonthDate = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth() - 1,
+      22,
+    ).toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+    const habitDrawer = page.getByTestId("habit-drawer");
+    await expect(habitDrawer).toBeVisible({ timeout: 5000 });
+    await expect(habitDrawer).toContainText(previousMonthDate);
   });
 });
