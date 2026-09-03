9dc8a8ecb2be2f112a829f60617ed9c04b4b5a3d
priya <priyaasinghh345@gmail.com>
2026-09-03 19:43:03 +0530

    add config to chakra provider to not apply its css across entire project

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,5 +1,12 @@
 import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react";
 
+// use this when you dont want chakra ui to reset css styles
+/*
+const system = createSystem(defaultConfig, {
+  cssVarsRoot: ":where(html)",
+  preflight: false, // disables the global reset
+});
+*/
 const system = createSystem(defaultConfig);
 
 export function Provider({ children }) {
